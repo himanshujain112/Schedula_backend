257c54ba9cb965394ae05713a1e9558862ade98e
Saraswat Chakraborty <145495612+Saraswat1@users.noreply.github.com>
2025-06-16 16:03:42 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getRootMessage(): string {
-    return 'Welcome to the NestJS Boilerplate!';
+    return 'Hello from Saraswat at PearlsThoughts Internship!';
   }
 
   getHelloMessage(): string {
